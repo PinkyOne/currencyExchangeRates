a0f3d9369182f80582a01542efdda85e484cc90d
Elistratov Alex <alexasnr56@gmail.com>
2019-05-03 01:45:45 +0400

    Added flow

--- src/pages/Converter/index.js
+++ src/pages/Converter/index.js
@@ -1,21 +1,22 @@
+// @flow
+
 import React, {Component} from "react";
 import {observable} from "mobx";
 import {observer} from "mobx-react";
 
 @observer
 class Converter extends Component {
-    @observable string;
+    @observable string: string = '';
 
-    render() {
+    render(): React$Node {
+        const {string} = this;
         return (<div>
-            <div>{this.string}</div>
+            <div>{string}</div>
             <input onChange={this.onChange}/>
         </div>);
     }
 
-    onChange = ({target: {value}}) => {
-        return this.string = value;
-    }
+    onChange = ({target: {value}}: SyntheticEvent) => this.string = value;
 }
 
 export default Converter;
