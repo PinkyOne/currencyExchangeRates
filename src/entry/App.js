5345c13c3692a2ee46e4317daea0f0e5bcde54d4
Elistratov Alex <alexasnr56@gmail.com>
2019-05-09 13:20:15 +0400

    Fixed production build CSS.

--- src/entry/App.js
+++ src/entry/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import JssProvider from 'react-jss/lib/JssProvider';
+import {createGenerateClassName} from '@material-ui/core/styles';
 
 import {StyledApp} from './styles';
 
+const generateClassName = createGenerateClassName({
+    dangerouslyUseGlobalCSS: false,
+    productionPrefix: 'c',
+});
+
 function App() {
     return (
-        <StyledApp>
-            <AppRouter/>
-        </StyledApp>
+        <JssProvider generateClassName={generateClassName}>
+            <StyledApp>
+                <AppRouter/>
+            </StyledApp>
+        </JssProvider>
     );
 }
 
